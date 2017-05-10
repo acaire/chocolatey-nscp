6374e79a00b29536457930d185823186c8d05694
Ash Caire <ash.caire@gmail.com>
2017-05-10 16:59:56 +0800

    Bump version to 0.5.0.62

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'NSClient++'
 $installerType = 'msi'
-$url = 'https://github.com/mickem/nscp/releases/download/0.4.4.19/NSCP-0.4.4.19-Win32.msi'
-$url64 = 'https://github.com/mickem/nscp/releases/download/0.4.4.19/NSCP-0.4.4.19-x64.msi'
+$url = 'https://github.com/mickem/nscp/releases/download/0.5.0.62/NSCP-0.5.0.62-Win32.msi'
+$url64 = 'https://github.com/mickem/nscp/releases/download/0.5.0.62/NSCP-0.5.0.62-x64.msi'
 
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
