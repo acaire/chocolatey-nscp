7b3a9a693a547623afd278a6c220ca0545952719
Ash Caire <ash.caire@gmail.com>
2017-05-16 21:10:23 +0800

    refactor powershell, add checksums

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,18 @@
 ﻿$packageName = 'NSClient++'
-$installerType = 'msi'
-$url = 'https://github.com/mickem/nscp/releases/download/0.5.0.62/NSCP-0.5.0.62-Win32.msi'
-$url64 = 'https://github.com/mickem/nscp/releases/download/0.5.0.62/NSCP-0.5.0.62-x64.msi'
+$url         = 'https://github.com/mickem/nscp/releases/download/0.5.0.62/NSCP-0.5.0.62-Win32.msi'
+$url64       = 'https://github.com/mickem/nscp/releases/download/0.5.0.62/NSCP-0.5.0.62-x64.msi'
 
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'msi'
+  url            = $url
+  url64bit       = $url64
+  silentArgs     = "/quiet"
+  validExitCodes = @(0)
+  checksum       = 'a9503ed8f3c9aa43aeda439ac39b302f155eea59a43c097fa8adb90770ec4c56'
+  checksumType   = 'sha256'
+  checksum64     = '7c5ae04dffd956cfbc6fee80a8fefdbe4f53ca6dbc9901e8995003a1ce75e03e'
+  checksumType64 = 'sha256'
+}
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage @packageArgs
