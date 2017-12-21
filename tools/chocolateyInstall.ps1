f0ef7bdd41233c3111169390e669ee7bc225d838
Ash Caire <ash.caire@gmail.com>
2017-12-21 23:27:59 +0800

    remove checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,10 +9,6 @@ $packageArgs = @{
   file           = gi $toolsDir\*Win32.msi
   file64         = gi $toolsDir\*x64.msi
   silentArgs     = "/quiet"
-  checksum       = 'AUTO_GENERATED'
-  checksumType   = 'sha256'
-  checksum64     = 'AUTO_GENERATED'
-  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
