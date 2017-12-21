d1dd538139f64b98f80775f5c69ea5f633339dcb
Ash Caire <ash.caire@gmail.com>
2017-12-21 23:39:12 +0800

    bar

--- tools/update.ps1
+++ tools/update.ps1
@@ -6,6 +6,7 @@ function global:au_SearchReplace {
     @{
        "$($Latest.PackageName).nuspec" = @{
           "(###VERSION###)" = "`${1}$($Latest.Version)"
+          "(\<version\>).*?(\</version\>)" = "`${1}$($Latest.Version)`$2"
         }
      }
 }
